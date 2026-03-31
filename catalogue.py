fe00825bcc14253fa47698981bf131e0ec624d52
Daniel BOKINGO <bokingodaniel4@gmail.com>
2026-03-31 16:04:19 +0200

    feat: add file reader with formatted display

--- catalogue.py
+++ catalogue.py
@@ -1,3 +1,3 @@
-with open('catalogue.txt', 'r') as file:
-    content = file.read()
-    print(content)
+with open("catalogue.txt", "r", encoding="utf-8") as fichier:
+    for ligne in fichier:
+        print(ligne.strip()) # strip() enleve le \n en fin de ligne
