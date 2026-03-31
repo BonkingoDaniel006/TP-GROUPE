cf92267e292f54f7b34a09e15d6a1f8191914c5b
Noémie Niamien <noemienmn@gmail.com>
2026-03-31 21:55:48 +0200

    contact-search-by-name

--- contacts.py
+++ contacts.py
@@ -4,3 +4,14 @@ def lire_contacts():
         for ligne in f:
             nom, tel = ligne.strip().split('|')
             print(f"Contact : {nom} (Tél: {tel})")
+
+def chercher_contact(nom_recherche):
+    """Recherche un contact par son nom"""
+    trouve = False
+    with open('contacts.txt', 'r') as f:
+        for ligne in f:
+            nom, tel = ligne.strip().split('|')
+            if nom_recherche.lower() in nom.lower():
+                print(f"Résultat : {nom} -> {tel}")
+                trouve = True
+    if not trouve: print("Aucun résultat.")
