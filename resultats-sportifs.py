6d573dfa008af4336b7a228b76b3064f2a2abdc1
Carlo Circo <Carlo.Circo@outlook.fr>
2026-03-31 21:33:46 +0200

    feat: add new expense and confirm write

--- resultats-sportifs.py
+++ resultats-sportifs.py
@@ -16,3 +16,15 @@ with open("depenses.csv", "r", encoding="utf-8") as fichier:
 print("Le total des dépenses est de : ", depenses_total)
 print("Le maximum des dépenses est de : ", depenses_max)
 print("Le minimum des dépenses est de : ", depenses_min)
+
+date = "2026-01-05"
+categorie = "informatique"
+description = "RTX 6090"
+montant = 3000
+with open("depenses.csv", "a", encoding="utf-8") as nvfichier:
+    nvfichier.write (f"{date}, {categorie}, {description},{montant}\n")
+
+with open("depenses.csv", "r", encoding="utf-8") as fichier:
+    lecture = csv.DictReader(fichier)
+    for ligne in lecture:
+        print(f"Date : {ligne['date']}, Catégorie : {ligne['categorie']}, Description : {ligne['description']}, Montant : {ligne['montant']}")
