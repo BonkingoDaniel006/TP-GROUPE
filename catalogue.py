dc9dedd5c1c823c816a1ce4390d416bee32394b6
Daniel BOKINGO <bokingodaniel4@gmail.com>
2026-03-31 23:56:22 +0200

    feat: add filter by rating and average calculation

--- catalogue.py
+++ catalogue.py
@@ -14,5 +14,17 @@ with open("catalogue.txt", 'r', encoding='utf-8') as f:
             realisateur= donnees[2]
             
             print(f"{nom} ({date}), réalisateur: {realisateur} note: {note}/10")
+        print("-------------------------------------------")
+        print("les mieux cotés sont:")
+        for ligne in lignes[1:]:
+            donnees = ligne.strip().split(';')
             
+            ind= (len(donnees))-1
+            note= int(donnees[ind])
+            nom= donnees[0]
+            date = donnees[1]
+            realisateur= donnees[2]
+            if note>= 9:
+                  
+                  print(f"{nom} ({date}), réalisateur: {realisateur} note: {note}/10")
             
