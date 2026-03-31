fcbb5fee0d3614eab7186b3f9b97140f90383c1e
Daniel BOKINGO <bokingodaniel4@gmail.com>
2026-03-31 23:30:48 +0200

    feat: add file reader with formatted display2

--- catalogue.py
+++ catalogue.py
@@ -1,3 +1,18 @@
-with open("catalogue.txt", "r", encoding="utf-8") as fichier:
-    for ligne in fichier:
-        print(ligne.strip()) # strip() enleve le \n en fin de ligne
+
+        
+with open("catalogue.txt", 'r', encoding='utf-8') as f:
+        lignes = f.readlines()
+        en_tete = lignes[0]
+        
+        for ligne in lignes[1:]: 
+            #on separe en liste
+            donnees = ligne.strip().split(';')
+            
+            note= (len(donnees))-1
+            nom= donnees[0]
+            date = donnees[1]
+            realisateur= donnees[2]
+            
+            print(f"{nom} ({date}), réalisateur: {realisateur} note: {note}/10")
+            
+            
