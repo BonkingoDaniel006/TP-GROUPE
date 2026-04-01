d43f889f0bbea378054dd8b8aa46b1106c7d1884
Carlo Circo <Carlo.Circo@outlook.fr>
2026-04-01 11:09:24 +0200

    feat: add pass count function

--- notes.py
+++ notes.py
@@ -2,4 +2,8 @@ notes = [12,8,15,9,17]
 def afficher_notes(liste):
     print("Notes :", liste)
 afficher_notes(notes)
-notes.append(19,14,11)
+notes.extend((19,15,11))
+print(notes)
+def calculer_moyenne(notes):
+    return sum(notes)//len(notes)
+print("La moyenne est de : ", (calculer_moyenne(notes)))
