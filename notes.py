281424d257dea027d7a9112139458944c374d303
Carlo Circo <Carlo.Circo@outlook.fr>
2026-04-01 11:30:12 +0200

    feat: add min max display function

--- notes.py
+++ notes.py
@@ -7,5 +7,7 @@ print(notes)
 def calculer_moyenne(notes):
     return sum(notes)//len(notes)
 print("La moyenne est de : ", (calculer_moyenne(notes)))
-print("Le maximum est : ", max(notes))
-print("Le minimum est : ", min(notes))
+def afficher_min_max(notes):
+    print("Le maximum est : ", max(notes))
+    print("Le minimum est : ", min(notes))
+print(afficher_min_max(notes))
