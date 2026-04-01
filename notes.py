601362dc53bbf607cf5288808b1f1ecb70ae937f
Noémie Niamien <noemienmn@gmail.com>
2026-04-01 11:45:46 +0200

    notes

--- notes.py
+++ notes.py
@@ -1,4 +1,13 @@
-notes = [12,8,15,9,17]
+notes = [12,8,15,9,17,11,19,7]
 def afficher_notes(liste):
-    print("Notes :", liste)
+    print("notes :", liste)
 afficher_notes(notes)
+
+def compter_reussites(liste) :
+        while notes>=10:
+            return ("notes :",liste)
+        else:
+            print ()
+compter_reussites()
+    
+    
